95efdbb5b48fc98474c924b14e5f60fdc8ee6ba6
Callon Campbell <calloncampbell@msn.com>
2015-06-07 23:07:40 -0400

    Cleanup

---
+++ assets/VersionInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("ReflectSoftware Inc.")]
+[assembly: AssemblyProduct("ReflectInsight")]
+[assembly: AssemblyCopyright("© 2014 ReflectSoftware Inc. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("ReflectInsight Samples")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyInformationalVersion("1.4.1")]
