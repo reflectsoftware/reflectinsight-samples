1a3abeed28821837611028cd16639163293195a4
Callon Campbell <calloncampbell@msn.com>
2015-06-28 11:34:45 -0400

    Updated VersionInfo.cs reference to point to assets folder. Added new listener samples: Console, Viewer, TextFile and BinaryFile.

---
+++ Listeners/ConsoleListener/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReflectSoftware.Insight;
+
+namespace ConsoleListener_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunSample();
+        }
+
+        static void RunSample()
+        {
+            while (true)
+            {
+                Console.WriteLine("Press any key to run test or press 'q' to quit...");
+
+                ConsoleKeyInfo k = Console.ReadKey();
+                if (k.KeyChar == 'q')
+                    break;
+                
+                RILogManager.Default.SendMessage("Message");
+                RILogManager.Default.SendDebug("Debug");
+                RILogManager.Default.SendInformation("Info");
+                RILogManager.Default.SendWarning("Warn");
+                RILogManager.Default.SendError("Error");
+                RILogManager.Default.SendFatal("Fatal");
+                RILogManager.Default.SendException("Exception", new Exception("Test"));
+                RILogManager.Default.SendMessage("Message");
+            }
+        }
+    }
+}
