42ac288337fd88d77c8043ae80f1327a26df1806
Callon Campbell <calloncampbell@msn.com>
2016-07-28 07:41:22 -0400

    Cleanup for presentation.

--- Getting Started/HelloReflectInsight/Program.cs
+++ Getting Started/HelloReflectInsight/Program.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using ReflectSoftware.Insight;
-
 namespace HelloReflectInsight_Sample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            RILogManager.Default.SendMessage("Hello, ReflectInsight!");
+
         }
     }
 }
